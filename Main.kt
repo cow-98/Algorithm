64e67ddd65c256f22069ca241b451b3866826640
junseon <sojh8908@naver.com>
2023-11-23 14:12:41 +0900

    백준 9498grade

---
+++ Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+        var score = readLine()!!.toInt()
+        when {
+                score in 90..100 -> println("A")
+                score in 80 until 90 -> println("B")
+                score in 70 until 80 -> println("C")
+                score in 60 until 70 -> println("D")
+                else -> println("F")
+        }
+}
