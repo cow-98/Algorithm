a1f241922da8c50afa66381162c59d689f061eb4
junseon <sojh8908@naver.com>
2023-12-23 00:00:46 +0900

    백준 10757 큰수 A + B

---
+++ APlusB.kt
@@ -0,0 +1,12 @@
+import java.math.BigInteger
+import java.util.Scanner
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val A = sc.nextBigInteger()
+    val B = sc.nextBigInteger()
+
+    val result = A + B
+
+    println(result)
+}
